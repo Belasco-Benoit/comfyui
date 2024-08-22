47b392a03b4f424485cac902816afa052ea98861
Benoit Belasco <72549524+Belasco-Benoit@users.noreply.github.com>
2024-08-22 18:24:06 +0200

    Create counter.py

---
+++ counter.py
@@ -0,0 +1,21 @@
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "increment": ("INT", {"default": 1, "min": 1, "max": 999})
+            }
+        }
+    
+    RETURN_TYPES = ("INT",)
+    FUNCTION = "count"
+    CATEGORY = "utils"
+
+    def count(self, increment):
+        self.count += increment
+        return (self.count,)
+
+NODE_CLASS_MAPPINGS["Counter"] = Counter
