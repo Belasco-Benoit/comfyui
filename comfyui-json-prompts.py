4f32fb17c313f38650d4a07ab6d317c6375d61cc
Benoit Belasco <72549524+Belasco-Benoit@users.noreply.github.com>
2024-08-23 17:01:35 +0200

    Update comfyui-json-prompts.py

--- comfyui-json-prompts.py
+++ comfyui-json-prompts.py
@@ -16,7 +16,7 @@ class JSONPromptLoader:
             "required": {
                 "json_file": ("STRING", {"default": "prompts.json"}),
                 "mode": (["sequential", "random"], {"default": "sequential"}),
-                "external_index": ("INT", {"forceInput": False, "default": 0, "min": 0, "max": 99999})
+                "external_index": ("INT", {"forceInput": True, "default": 0, "min": 0, "max": 99999})
                 
             }
         }
